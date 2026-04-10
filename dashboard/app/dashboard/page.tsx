c5fa74a28b556b275ac1a310735a28885adb917a
netproind <131628704+netproind@users.noreply.github.com>
2026-04-10 01:27:38 -0400

    Update page.tsx

--- dashboard/app/dashboard/page.tsx
+++ dashboard/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
+export const revalidate = 0
 export default async function DashboardPage() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
