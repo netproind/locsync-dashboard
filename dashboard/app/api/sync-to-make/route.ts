963b1222e0e64896f3b8468d0fb1d0016ca41cf3
netproind <131628704+netproind@users.noreply.github.com>
2026-04-15 02:41:58 -0400

    Update route.ts

--- dashboard/app/api/sync-to-make/route.ts
+++ dashboard/app/api/sync-to-make/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: Request) {
   console.log('sync-to-make called')
   const body = await req.json()
   
-  await fetch('https://hook.us2.make.com/g3ye50nir54f1ps7cf7xh7ifpor32lys', {
+  await fetch('https://hook.us2.make.com/plojf12wui5wxzo04u9p75g5xvwynee7', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
